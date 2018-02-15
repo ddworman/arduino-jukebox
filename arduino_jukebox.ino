ceef1da843ce6b49550d1e08301683828298fb63
ddworman <35926101+ddworman@users.noreply.github.com>
2018-02-15 09:14:30 -0500

    this plays music

---
+++ arduino_jukebox.ino
@@ -0,0 +1,73 @@
+#include <BlockDriver.h>
+#include <FreeStack.h>
+#include <MinimumSerial.h>
+#include <SdFat.h>
+#include <SdFatConfig.h>
+#include <SysCall.h>
+
+#include <SFEMP3Shield.h>
+#include <SFEMP3ShieldConfig.h>
+#include <SFEMP3Shieldmainpage.h>
+
+//Simple example of using Sparkfun's MP3 Shield. You need to install the SdFat and SFEMP3Shield libraries first.
+
+//Select include SdFat library from Sketch menu 
+
+
+
+
+
+int trackNumber = 1;
+
+SdFat sd; // Create SDFat object. 
+
+SFEMP3Shield MP3player; // Create SFEMP3Shield object
+
+//Settings for MP3 player
+const uint8_t volume = 0; // MP3 Player volume 0=max, 255=lowest (off)
+const uint16_t monoMode = 1;  // Mono setting 0=off, 3=max
+
+
+//In void setup you need to functions which are define below
+void setup(){
+  initSD();  // Initialize the SD card
+  initMP3Player(); // Initialize the MP3 Shield
+}
+
+void loop()
+{
+
+
+  
+  MP3player.playTrack(1); //plays track 1 (track001.mp3)
+ }
+ 
+ 
+
+//function to intialize SD card. Leave it alone unless you know what you're doing.
+
+void initSD()
+{
+  //Initialize the SdCard.
+  if(!sd.begin(SD_SEL, SPI_HALF_SPEED)) 
+    sd.initErrorHalt();
+  if(!sd.chdir("/")) 
+    sd.errorHalt("sd.chdir");
+}
+
+
+//function to intialize MP3 player. Leave it alone unless you know what you're doing
+
+void initMP3Player()
+{
+  uint8_t result = MP3player.begin(); // init the mp3 player shield
+  if(result != 0) // check result, see readme for error codes.
+  {
+    // Error checking can go here!
+  }
+  MP3player.setVolume(volume, volume);
+  MP3player.setMonoMode(monoMode);
+}
+
+
+
